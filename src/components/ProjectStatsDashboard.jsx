331d7859d1f7c9bebba6830cff259c13cd166ebf
unknown <skanthale9@gmail.com>
2025-04-19 15:45:57 +0530

    header components fix

--- src/components/ProjectStatsDashboard.jsx
+++ src/components/ProjectStatsDashboard.jsx
@@ -9,7 +9,6 @@ const ProjectStatsDashboard = () => {
   return (
     <div className="py-3 w-full">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {/* Total Project Card */}
         <div className="bg-white rounded-lg shadow-md p-5 flex justify-between items-start">
           <div>
             <h3 className="text-gray-400 text-sm font-medium mb-1">
@@ -37,24 +36,8 @@ const ProjectStatsDashboard = () => {
               <span className="text-gray-400 text-xs ml-1">+$150 today</span>
             </div>
           </div>
-          {/* <div className="rounded-full bg-blue-100 p-2">
-            <svg
-              className="w-5 h-5 text-blue-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </div> */}
           <Image src={cardoneicon} alt="" height={"auto"} width={"auto"} /> 
         </div>
-
-        {/* In Progress Card */}
         <div className="bg-white rounded-lg shadow-sm p-5 flex justify-between items-start">
           <div>
             <h3 className="text-gray-400 text-sm font-medium mb-1">
@@ -84,8 +67,6 @@ const ProjectStatsDashboard = () => {
           </div>
           <Image src={cardicontwo} alt="" height={"auto"} width={"auto"} /> 
         </div>
-
-        {/* Finished Card */}
         <div className="bg-white rounded-lg shadow-sm p-5 flex justify-between items-start">
           <div>
             <h3 className="text-gray-400 text-sm font-medium mb-1">Finished</h3>
@@ -113,8 +94,6 @@ const ProjectStatsDashboard = () => {
           </div>
           <Image src={cardiconthree} alt="" height={"auto"} width={"auto"} /> 
         </div>
-
-        {/* Unfinished Card */}
         <div className="bg-white rounded-lg shadow-sm p-5 flex justify-between items-start">
           <div>
             <h3 className="text-gray-400 text-sm font-medium mb-1">
